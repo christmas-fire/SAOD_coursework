2544d4b7377a1dd69403893079a7ccb57642f2f5
Бабешко Александр Владимирович <vpiskin335@gmail.com>
2024-09-12 19:18:04 +0700

    12.09.24

--- list.cpp
+++ list.cpp
@@ -3,9 +3,28 @@
 
 using namespace std;
 
-
-
+struct Node {
+    int value;
+    Node* next;
+};
 
 int main() {
+
+    Node *first = new Node;
+    first->next = nullptr;
+    first->value = 3;
+
+    Node *second = new Node;
+    second->next = nullptr;
+    second->value = 1;
+
+    Node *third = new Node;
+    third->next = nullptr;
+    third->value = 5;
+
+    first->next = second;
+    second->next = third;
+
+    cout << first->value;
     return 0;
 }
